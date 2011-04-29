d65b229834927bd1b12a1e605c8761cb3e3b2612
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2011-04-28 22:52:49 -0700

    Initial commit

---
+++ hackertyper.js
@@ -0,0 +1,49 @@
+var isTyping = false;
+var codePane;
+var charPerStroke = 3;
+var position;
+var text;
+function init(){
+	document.getElementById("go-button").onclick=start;
+	codePane = document.getElementById("code-pane");
+}
+function start(){
+	document.getElementById("pref-form").style.display = "none";
+	document.getElementById("code-form").style.display = "";
+	charPerStroke=parseInt(document.getElementById("keystroke-chars").value);
+	var fileToLoad = document.getElementById("select-text-input").value;
+	if(fileToLoad == "CUSTOM"){
+		loadCustomFile();
+	}
+	else{
+		loadFile(fileToLoad);
+	}
+}
+function loadFile(fileToLoad){
+	var req = new XMLHttpRequest();
+	req.open("GET", fileToLoad, false);
+	req.send(null);
+	text = req.responseText;
+	ready();
+}
+function loadCustomFile(){
+}
+function ready(){
+	isTyping=true;
+	position = 0;
+	codePane.textContent = "";
+}
+function keyHandler(e){
+	if(!isTyping){
+		return true;
+	}
+	position+=charPerStroke;
+	if(position>text.length){
+		position = text.length;
+	}
+	codePane.textContent = text.substr(0, position);
+	window.scrollTo(0, document.body.scrollHeight);
+	return false;
+}
+window.onload=init;
+window.onkeyup=keyHandler;
